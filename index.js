ed35e884dcbdfb203396a68b7bd2bea80918ccf8
lateepharh <pelumilateefat299@gmail.com>
2022-09-25 02:26:47 -0700

    committed

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ btn.forEach((item,i)=>{
       checked= i;
 
    })
-})   
+}) 
+const barIcon = document.getElementById('bar')
+const closeIcon = document.getElementById('close')
+const nav = document.getElementById('navbar')
+
+// CLICK EVENTS
+
+// HAMBURGER OPEN
+if (bar) {
+    barIcon.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+// HAMBURGER CLOSE
+if (bar) {
+    closeIcon.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}  
