3e223d55ae159d32810305d8d9eb028ab733555e
lateepharh <pelumilateefat299@gmail.com>
2022-10-13 10:03:10 -0700

    committed

--- index.js
+++ index.js
@@ -8,16 +8,4 @@ btn.forEach((item,i)=>{
 
    })
 }) ;
-var dropdown = document.getElementsByClassName("dropdown");
-var i;
-for(i = 0; i < dropdown.length; i++){
-    dropdown[i].addEventListener("click", ()=>{
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display === "none";
-        }else{
-            content.style.display === "block";
-        }
-    })
-}
+// 
